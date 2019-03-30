db17f3cbb573080745f5508dfe5ac9681819c382
Albert Gafiyatullin <binixary@gmail.com>
2019-03-30 22:23:46 +0700

    OS UNIX+ System V: rewrited laboratory work №11

--- OS UNIX+ System V/Process creation and programs execution/laboratory work #11/src/main.c
+++ OS UNIX+ System V/Process creation and programs execution/laboratory work #11/src/main.c
@@ -1,3 +1,7 @@
+//
+// Created by xp10rd on 3/30/19.
+//
+
 #include <stdio.h>
 #include "execvpe.h"
 
@@ -5,7 +9,8 @@ extern char **environ;
 
 int main(int argc, char *argv[]) {
     printf("Process environment variables:\n");
-    for(int i = 0; environ[i] != NULL; i++)
+    int i;
+    for(i = 0; environ[i] != NULL; i++)
         printf("%s\n", environ[i]);
 
     char *envp[] = {"TEST=VARIABLE", "ALL=WORKS", "ABSOLUTELY=NORMALLY", NULL};
@@ -15,7 +20,7 @@ int main(int argc, char *argv[]) {
     }
 
     printf("Process environment variables after unsuccessful execvpe:\n");
-    for(int i = 0; environ[i] != NULL; i++)
+    for(i = 0; environ[i] != NULL; i++)
         printf("%s\n", environ[i]);
 
     return -1;
--- OS UNIX+ System V/Process creation and programs execution/laboratory work #11/src/test.c
+++ OS UNIX+ System V/Process creation and programs execution/laboratory work #11/src/test.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 extern char **environ;
-int main() {
+
+int main(int argc, char *argv[], char *envp[]) {
     printf("Process environment variables after successful execvpe:\n");
-    for(int i = 0; environ[i] != NULL; i++)
-        printf("%s\n", environ[i]);
+    int i;
+    for(i = 0; envp[i] != NULL; i++)
+        printf("%s\n", envp[i]);
+
+    printf("Process arguments after successful execvpe:\n");
+    for(i = 0; i < argc; i++)
+        printf("%s\n", argv[i]);
 
     return 0;
 }
