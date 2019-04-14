e2d06798ceae457e9a66c8e5fdbed8a2a88d8a21
Albert Gafiyatullin <binixary@gmail.com>
2019-04-14 23:43:01 +0700

    OS UNIX+ System V:done the laboratory works #21, #22

--- OS UNIX+ System V/Signals/laboratory work #21/src/main.c
+++ OS UNIX+ System V/Signals/laboratory work #21/src/main.c
@@ -3,26 +3,29 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define BEL '\07'
+#define BELL '\07'
 
-static void beep(int);
+void beep(int);
 
 int main() {
-    signal(SIGINT, beep);
-    signal(SIGQUIT, beep);
+    struct sigaction act;
+    act.sa_handler = beep;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = SA_INTERRUPT;
+    sigaction(SIGQUIT, &act, NULL);
+    sigaction(SIGINT, &act, NULL);
     for(;;)
         pause();
 }
 
 
-static void beep(int signo) {
+void beep(int signo) {
     static int counter = 0;
     if(signo == SIGINT) {
-        printf("%c\n", BEL);
+        printf("%c\n", BELL);
         counter++;
     } else {
         printf("Beep counter: %d\n", counter);
         exit(0);
     }
-    signal(SIGINT, beep);
 }
---
+++ OS UNIX+ System V/Signals/laboratory work #22/src/main.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+#define TIME_OUT 5 //time in seconds
+#define STRINGSIZ 256
+
+int open_files(int *files, int num, char **paths);
+
+int multiplexed_read(int *files, int num);
+
+int main(int argc, char *argv[]) {
+    if(argc < 2) {
+        return EINVAL;
+    }
+    int *files = (int*)malloc(sizeof(int) * MIN((argc - 1), _POSIX_OPEN_MAX));
+    if(open_files(files, argc - 1, argv + 1) == -1) {
+        return errno;
+    }
+
+    if(multiplexed_read(files, argc - 1) == -1) {
+        return errno;
+    }
+    free(files);
+    return 0;
+}
+
+int open_files(int *files, int num, char **paths) {
+    int i;
+    for(i = 0; i < num; i++) {
+        if((files[i] = open(paths[i], O_RDONLY)) == -1) {
+            perror("Can't open the file");
+            return -1;
+        }
+    }
+    return 0;
+}
+
+void sig_alarm(int signum) { }
+
+int multiplexed_read(int *files, int num) {
+    /* set signal handler */
+    struct sigaction act;
+    act.sa_handler = sig_alarm;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = SA_INTERRUPT;
+    sigaction(SIGALRM, &act, NULL);
+    /* main part of the function */
+    char buffer[STRINGSIZ];
+    int current_file = -1, opened_num = num, length;
+    while(opened_num) {
+        if(++current_file == num) {
+            current_file = 0;
+        }
+        if(files[current_file] == -1) {
+            continue;
+        }
+        alarm(TIME_OUT);    //set alarm
+        if((length = read(files[current_file], buffer, STRINGSIZ)) == -1) {
+            alarm(0);   //stop alarm
+            if(errno == EINTR) {
+                continue;
+            } else {
+                files[current_file] = -1;
+                opened_num--;
+                close(files[current_file]);
+            }
+        } else {
+            alarm(0);   //stop alarm
+            //write to stdout
+            if(write(1, buffer, length) == -1) {
+                perror("Can't write to stdout");
+                return -1;
+            }
+        }
+    }
+    return 0;
+}
